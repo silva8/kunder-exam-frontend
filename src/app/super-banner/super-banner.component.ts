0d552288a8bdf5f440fb99b38284ef8500da3a3b
christobal <cristobal.silva@riotsecure.se>
2020-12-26 12:26:14 -0300

    [FEAT(component)]: Banner component created

---
+++ src/app/super-banner/super-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-super-banner',
+  templateUrl: './super-banner.component.html',
+  styleUrls: ['./super-banner.component.scss']
+})
+export class SuperBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
