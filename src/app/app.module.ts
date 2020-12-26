ee431f59f9e5b4ed03a769420f1f72d307d7b79c
christobal <cristobal.silva@riotsecure.se>
2020-12-26 18:12:41 -0300

    [FIX]: httpClient added to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { BannerService } from './banner.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from '../components/home/home.component';
+import { SuperBannerComponent } from '../components/super-banner/super-banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SuperBannerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BannerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
