1876e503cbe244cab8c2ec4eed4e8ffc0bf5151d
christobal <cristobal.silva@riotsecure.se>
2020-12-26 18:32:55 -0300

    [FIX]: Banner-component moved to components directory

---
+++ src/components/super-banner/super-banner.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { BannerService } from '../../app/banner.service';
+
+@Component({
+  selector: 'app-super-banner',
+  templateUrl: './super-banner.component.html',
+  styleUrls: ['./super-banner.component.scss']
+})
+export class SuperBannerComponent implements OnInit {
+
+  banner;
+
+  constructor(private bannerService: BannerService) { }
+
+  ngOnInit() {
+    this.getBanner();
+  }
+
+  async getBanner() {
+    let data = await this.bannerService.getBannerData().toPromise();
+    this.banner = data['product'];
+  }
+
+}
